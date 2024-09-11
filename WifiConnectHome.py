23c045f7930d5aaf782619d8c033774f3ecc250e
arielfelcan <93785806+arielfelcan@users.noreply.github.com>
2024-09-11 00:28:33 -0400

    Update WifiConnectHome.py

--- WifiConnectHome.py
+++ WifiConnectHome.py
@@ -5,7 +5,7 @@ def WifiConnectHome():
     
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect('28Belknap2', 'Pisinski12!')
+    wlan.connect('farty', 'Bigchungus4040')
 
     while wlan.ifconfig()[0] == '0.0.0.0':
         print('.', end=' ')
